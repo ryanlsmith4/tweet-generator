29ce76217f1b6cecc4d5e62c76d0cf6f09e1d635
Ryan Smith <ryan.l.smith4@gmail.com>
2019-01-28 17:09:56 -0800

    rearrange method

---
+++ rearrange.py
@@ -0,0 +1,14 @@
+import random
+import sys
+def rearrange():
+    words = sys.argv[1:]
+    neworder = []
+    length=len(words)
+    for i in range(length):
+       num = random.randint(0, len(words)-1)
+       neworder.append(words[num])
+       words.pop(num)
+    print (*neworder)
+    
+if __name__ == '__main__':
+    rearrange()
