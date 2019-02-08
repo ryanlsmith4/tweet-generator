44bbad496f4e3a4c9ad53b5de1e147a001463cdb
Ryan Smith <ryan.l.smith4@gmail.com>
2019-02-08 11:49:02 -0800

    hdeploying to heroku

--- sampler.py
+++ sampler.py
@@ -8,10 +8,11 @@ def test_sample():
     temp_word_list = []
     freq = {}
     manifesto = get_word_list()
-    lol = lists_of_list(manifesto)
+    # Uncomment this after testing
+    l_o_l_man= lists_of_list(manifesto)
 
     for i in range(0, 1000):
-        temp_word_list.append(sample(lol))
+        temp_word_list.append(sample(l_o_l_man))
 
     for word in temp_word_list:
         if word not in freq:
@@ -19,24 +20,34 @@ def test_sample():
         else:
             freq[word] += 1
     for key, value in freq.items():
-        return(key,format(value/len(temp_word_list), '.4f'))
+        return freq
+        # return(key,format(value/len(temp_word_list), '.4f'))
 
 def sample(list_of_list):
     ''' Function to take in a list of list and return a random value
         based on weight'''
-    weight = 0
+    # Tokens equals the total number of words
+    # Types equals the total number of unique words
+    tokens = 0
     cum_prob = 0.0
-    for word_count in list_of_list:
-        weight += word_count[1]
+    for inner_list in list_of_list:
+        tokens += inner_list[1]
+        # print('Holla {}'.format(inner_list))
 
+    # random.uniform returns a float between 0 and .99
     rand_num = random.uniform(0,1)
-    for word_count in list_of_list:
-        cum_prob += word_count[1]/weight
-        if cum_prob >= rand_num:
-            # print(word_count[0])
-            return word_count[0]
+    for inner_list in list_of_list:
+        cum_prob += inner_list[1]/tokens
+        # print('Prob {}'.format(cum_prob))
+        # print('Prob {}'.format(cum_prob))
 
+        if cum_prob >= rand_num:
+            # print(inner_list[0])
+            # print('Weighted word {}'.format(inner_list[0]))
+            return inner_list[0]
 
 
 if __name__ == '__main__':
-    test_sample()
+    print(test_sample())
+    # lol = lists_of_list(words)
+    # print(sample(lol))
