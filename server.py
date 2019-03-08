8206d1f09d019d5dd7a8f1a0fd6eee65e8261bfb
Ryan Smith <ryan.l.smith4@gmail.com>
2019-03-08 14:03:17 -0800

    added required args

--- server.py
+++ server.py
@@ -1,8 +1,15 @@
 from flask import Flask, jsonify, render_template
-from sentence import gen_sentence
+from sentence import gen_sentence,
+from dictogram import Dictogram
+from cleanup import get_word_list
+
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_word():
-    words = gen_sentence()
+    word_list = get_word_list()
+    histogram = Dictogram(word_list)
+    words = gen_sentence(histogram, word_list)
     return  render_template('index.html', words = words)
