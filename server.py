a06e0cdd9e40be9c6fe99f67c039db80d201466c
Ryan Smith <ryan.l.smith4@gmail.com>
2019-03-08 14:13:14 -0800

    changed filename

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, render_template
-from sentence import gen_sentence
+from markov import gen_sentence
 from dictogram import Dictogram
 from cleanup import get_word_list
 
